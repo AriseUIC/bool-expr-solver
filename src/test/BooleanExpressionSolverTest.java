091c9cf68242e784f651d2d0f1bb9bc6a173e991
kart1618 <kart1618@gmail.com>
2014-02-13 12:22:07 -0600

    Initial Commit with test case. Coverage - 47.4%

---
+++ src/test/BooleanExpressionSolverTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import net.jbouchard.bool.BooleanExpressionSolver;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class BooleanExpressionSolverTest {
+
+	ByteArrayOutputStream systemOut = new ByteArrayOutputStream();
+	
+	@Test
+	public void test() {
+		BooleanExpressionSolver bes = new BooleanExpressionSolver();
+		try {
+			
+			System.setOut(new PrintStream(systemOut));
+			
+			String[] argv={"-b","-e","A|B"};
+			bes.main(argv);
+			String result=systemOut.toString();
+			
+			Assert.assertEquals("A B  A|B \n0 0   0  \n0 1   1  \n1 0   1  \n1 1   1",result.trim());
+			systemOut.close();
+		}
+		catch (Exception ex) {
+			
+			fail("Unexpected"+ex.getStackTrace());
+		}
+		//fail("Not yet implemented");
+	}
+
+}
