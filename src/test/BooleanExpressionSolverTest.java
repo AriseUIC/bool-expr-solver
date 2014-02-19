04e24eaed08463247d2ce7a62c8dedbc8e93b792
kart1618 <kart1618@gmail.com>
2014-02-18 20:53:51 -0600

    Coverage: 54%%

--- src/test/BooleanExpressionSolverTest.java
+++ src/test/BooleanExpressionSolverTest.java
@@ -58,4 +58,88 @@ public class BooleanExpressionSolverTest {
 		//fail("Not yet implemented");
 	}
 
+
+	@Test
+	public void test_not() {
+		BooleanExpressionSolver bes = new BooleanExpressionSolver();
+		try {
+			
+			System.setOut(new PrintStream(systemOut));
+			
+			String[] argv={"-b","-e","~A"};
+			bes.main(argv);
+			String result=systemOut.toString();
+			
+			Assert.assertEquals("A  ~A \n0  1  \n1  0",result.trim());
+			systemOut.close();
+		}
+		catch (Exception ex) {
+			
+			fail("Unexpected"+ex.getStackTrace());
+		}
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_equals() {
+		BooleanExpressionSolver bes = new BooleanExpressionSolver();
+		try {
+			
+			System.setOut(new PrintStream(systemOut));
+			
+			String[] argv={"-b","-e","A=B"};
+			bes.main(argv);
+			String result=systemOut.toString();
+			
+			Assert.assertEquals("A B  A=B \n0 0   1  \n0 1   0  \n1 0   0  \n1 1   1",result.trim());
+			systemOut.close();
+		}
+		catch (Exception ex) {
+			
+			fail("Unexpected"+ex.getStackTrace());
+		}
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_xor() {
+		BooleanExpressionSolver bes = new BooleanExpressionSolver();
+		try {
+			
+			System.setOut(new PrintStream(systemOut));
+			
+			String[] argv={"-b","-e","A+B"};
+			bes.main(argv);
+			String result=systemOut.toString();
+			
+			Assert.assertEquals("A B  A+B \n0 0   0  \n0 1   1  \n1 0   1  \n1 1   0",result.trim());
+			systemOut.close();
+		}
+		catch (Exception ex) {
+			
+			fail("Unexpected"+ex.getStackTrace());
+		}
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_less_than() {
+		BooleanExpressionSolver bes = new BooleanExpressionSolver();
+		try {
+			
+			System.setOut(new PrintStream(systemOut));
+			
+			String[] argv={"-b","-e","A>B"};
+			bes.main(argv);
+			String result=systemOut.toString();
+			
+			Assert.assertEquals("A B  A>B \n0 0   1  \n0 1   1  \n1 0   0  \n1 1   1",result.trim());
+			systemOut.close();
+		}
+		catch (Exception ex) {
+			
+			fail("Unexpected"+ex.getStackTrace());
+		}
+		//fail("Not yet implemented");
+	}
 }
