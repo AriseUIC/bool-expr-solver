bacaa9628a8bba44b01a54f4fcc96b57cbabb798
kart1618 <kart1618@gmail.com>
2014-03-20 13:36:38 -0500

    Added a folder for UML Diagrams. Completed Usecase Diagram.

--- src/test/BooleanExpressionSolverTest.java
+++ src/test/BooleanExpressionSolverTest.java
@@ -143,24 +143,5 @@ public class BooleanExpressionSolverTest {
 		//fail("Not yet implemented");
 	}
 	
-	@Test
-	public void expressions_test_1() {
-		BooleanExpressionSolver bes = new BooleanExpressionSolver();
-		try {
-			
-			System.setOut(new PrintStream(systemOut));
-			
-			String[] argv={"-b","-e","A&B|C"};
-			bes.main(argv);
-			String result=systemOut.toString();
-			
-			Assert.assertEquals("A B C  A&B  A&B|C \n0 0 0   0     0   \n0 0 1   0     1   \n0 1 0   0     0   \n0 1 1   0     1   \n1 0 0   0     0   \n1 0 1   0     1   \n1 1 0   1     1   \n1 1 1   1     1",result.trim());
-			systemOut.close();
-		}
-		catch (Exception ex) {
-			
-			fail("Unexpected"+ex.getStackTrace());
-		}
-		//fail("Not yet implemented");
-	}
+	
 }
