7e10f27bb09d60940fa4ace215c65771c871001c
kart1618 <kart1618@gmail.com>
2014-03-20 09:39:21 -0500

    Added a few more tests. Coverage - 61.6%

--- src/test/ComputeExpression.java
+++ src/test/ComputeExpression.java
@@ -14,7 +14,7 @@ public class ComputeExpression {
 
 	ByteArrayOutputStream systemOut = new ByteArrayOutputStream();
 	@Test
-	public void expressions_test_1() {
+	public void expressions_test() {
 		BooleanExpressionSolver bes = new BooleanExpressionSolver();
 		try {
 			
@@ -32,6 +32,24 @@ public class ComputeExpression {
 			fail("Unexpected"+ex.getStackTrace());
 		}
 		//fail("Not yet implemented");
+	}
+	@Test
+	public void expressions_with_brackets_test() {
+		BooleanExpressionSolver bes = new BooleanExpressionSolver();
+		try {
+			
+			System.setOut(new PrintStream(systemOut));
+			
+			String[] argv={"-b","-e","A&(B|C)"};
+			bes.main(argv);
+			String result=systemOut.toString();
+			
+			Assert.assertEquals("A B C  B|C  A&(B|C) \n0 0 0   0      0    \n0 0 1   1      0    \n0 1 0   1      0    \n0 1 1   1      0    \n1 0 0   0      0    \n1 0 1   1      1    \n1 1 0   1      1    \n1 1 1   1      1",result.trim());
+			systemOut.close();
+		}
+		catch (Exception ex) {
+			fail("Unexpected"+ex.getStackTrace());
+		}
 	}
 
 }
