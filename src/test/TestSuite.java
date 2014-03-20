cf313752e8048bfea14879f0fb565682aaeedac8
kart1618 <kart1618@gmail.com>
2014-03-20 00:00:49 -0500

    Added TestSuite and a few more tests. Coverage - 57.7%

---
+++ src/test/TestSuite.java
@@ -0,0 +1,20 @@
+package test;
+
+import net.jbouchard.bool.BooleanExpressionSolver;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	BooleanExpressionSolverTest.class,
+	ComputeExpression.class,
+})
+public class TestSuite {
+	
+}
+
+
